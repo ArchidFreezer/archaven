eab3d37989ee925cb3d9b4b8432e15e7ba5e7fdf
Archid <0archid0@gmail.com>
2025-10-14 13:18:55 +0100

    Add script to generate template build file

---
+++ tools/create-build-template.py
@@ -0,0 +1,283 @@
+#!/home/archid/python/.archaven/bin/python
+
+# Open the output json file
+hf = open(f"build-template.json", "w")
+
+hf.write('''{
+	"builds": [
+		{
+			"id": "",
+			"classid": "",
+			"build": "",
+			"orig_author": "",
+			"orig_url": "",
+			"overview": { 
+				"body": [
+					"",
+					""
+				],
+				"benefits": [
+					"",
+					""
+				],
+				"preferences": [
+					"",
+					""
+				]
+			},
+			"openers": [
+				{
+					"label": "",
+					"level": x,
+					"overview": "",
+					"rounds": [
+						{
+							"top": "cardid",
+							"bottom": "cardid",
+							"strategy": ""
+						},
+												{
+							"top": "cardid",
+							"bottom": "cardid",
+							"strategy": ""
+						}
+					]
+				}
+			],
+			"cards": [
+				{
+					"id": "cardid",
+					"top": "",
+					"bottom": "",
+					"overall": ""
+				},
+				{
+					"id": "cardid",
+					"top": "",
+					"bottom": "",
+					"overall": ""
+				}
+			]
+			"levels": [
+				{
+					"level": x,
+					"choices": [
+						{
+							"label": "Core",
+							"overview": "",
+							"cards": [
+								{
+									"id": "cardid",
+									"comment": ""
+								},
+								{
+									"id": "cardid",
+									"comment": ""
+								}
+							]
+						},
+						{
+							"label": "",
+							"overview": "",
+							"cards": [
+								{
+									"id": "cardid",
+									"comment": ""
+								},
+								{
+									"id": "cardid",
+									"comment": ""
+								}
+							]
+						}
+					],
+					"picks": [
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": ""
+							},
+							"bottom": {
+								"text": "",
+								"style": ""
+							},
+							"replace": "cardid",
+							"comment": ""
+						},
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": ""
+							},
+							"bottom": {
+								"text": "",
+								"style": ""
+							},
+							"replace": "cardid",
+							"comment": ""
+						}
+					],
+					"hand": [
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": "w3-pale-green"
+							},
+							"bottom": {
+								"text": "",
+								"style": "w3-pale-red"
+							}
+						},
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": "w3-pale-yellow"
+							},
+							"bottom": {
+								"text": "",
+								"style": "w3-pale-blue"
+							}
+						}
+					],
+					"sideboard": [
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": ""
+							},
+							"bottom": {
+								"text": "",
+								"style": ""
+							},
+							"replace": "cardid",
+							"comment": "on this condition"
+						}
+					]
+				}
+			],
+			"perks": [
+				{
+					"order": 1,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 2,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 3,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 4,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 5,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 6,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 7,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 8,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 9,
+					"name": "",
+					"effect": ""
+				},
+				{
+					"order": 10,
+					"name": "",
+					"effect": ""
+				}
+			],
+			"items": {
+				"comments": "",
+				"levels": [
+					{
+						"level": "Starting",
+						"comments": "",
+						"picks": [
+							{
+								"id": "itemid",
+								"comment": ""
+							},
+							{
+								"id": "itemid",
+								"comment": ""
+							}
+						]
+					},
+					{
+						"level": "Craftsman 1",
+						"comments": "",
+						"picks": [
+							{
+								"id": "itemid",
+								"comment": ""
+							},
+							{
+								"id": "itemid",
+								"comment": ""
+							}
+						]
+					}
+				]
+			},
+			"potions": {
+				"comments": "",
+				"levels": [
+					{
+						"numherbs": "2",
+						"comments": "",
+						"picks": [
+							{
+								"id": "itemid",
+								"comment": ""
+							},
+							{
+								"id": "itemid",
+								"comment": ""
+							}
+						]
+					},
+					{
+						"numherbs": "",
+						"comments": "",
+						"picks": [
+							{
+								"id": "itemid",
+								"comment": ""
+							},
+							{
+								"id": "itemid",
+								"comment": ""
+							}
+						]
+					}
+				]
+			}
+		}
+	]
+}''')

06730b377ab34305ce0316831abd13ec491b7ecf
Archid <0archid0@gmail.com>
2025-10-21 14:26:44 +0100

    Update template creation script to allow intermediate testing
    The script to construct the build template json file created all parts, but made it awkward to test the build structure iteratively.
    This commit updates the script to create an additional shortened template file only containing the core elements of a build id is passed in. This allows the shortened template be used one level at a time to allow visualisation of the hand. The full template is still created so may be used to copy in additional blocks as required.

--- tools/build-template-create.py
+++ tools/build-template-create.py
@@ -1,9 +1,126 @@
 #!/home/archid/python/.archaven/bin/python
 
-# Open the output json file
-hf = open(f"build-template.json", "w")
+import argparse
 
-hf.write('''{
+def write_build_file(fh):
+	fh.write('''{
+	"builds": [
+		{
+			"id": "",
+			"classid": "",
+			"build": "",
+			"orig_author": "",
+			"orig_url": "",
+			"overview": { 
+				"body": [
+					"",
+					""
+				],
+				"benefits": [
+					"",
+					""
+				],
+				"preferences": [
+					"",
+					""
+				]
+			},
+			"cards": [
+				{
+					"id": "cardid",
+					"top": "",
+					"bottom": "",
+					"overall": ""
+				},
+				{
+					"id": "cardid",
+					"top": "",
+					"bottom": "",
+					"overall": ""
+				}
+			],
+			"levels": [
+				{
+					"level": 1,
+					"handcomment": "",
+					"choices": [
+						{
+							"label": "Core",
+							"overview": "",
+							"cards": [
+								{
+									"id": "cardid",
+									"comment": ""
+								},
+								{
+									"id": "cardid",
+									"comment": ""
+								}
+							]
+						},
+						{
+							"label": "",
+							"overview": "",
+							"cards": [
+								{
+									"id": "cardid",
+									"comment": ""
+								},
+								{
+									"id": "cardid",
+									"comment": ""
+								}
+							]
+						}
+					],
+					"hand": [
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": "w3-pale-green"
+							},
+							"bottom": {
+								"text": "",
+								"style": "w3-pale-red"
+							}
+						},
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": "w3-pale-yellow"
+							},
+							"bottom": {
+								"text": "",
+								"style": "w3-pale-blue"
+							}
+						}
+					],
+					"sideboard": [
+						{
+							"card": "cardid",
+							"top": {
+								"text":"",
+								"style": ""
+							},
+							"bottom": {
+								"text": "",
+								"style": ""
+							},
+							"replace": "cardid",
+							"comment": "on this condition"
+						}
+					]
+				}
+			]
+		}
+	]
+}''')
+
+
+def write_template_file(fh):
+	fh.write('''{
 	"builds": [
 		{
 			"id": "",
@@ -319,3 +436,21 @@ hf.write('''{
 		}
 	]
 }''')
+
+
+parser = argparse.ArgumentParser(
+                    prog='build-template-create',
+                    description='Create a template file for the creation of frosthaven builds')
+parser.add_argument('-b', help='build id being created')
+args = parser.parse_args()
+
+
+# Open the output json file
+hf = open("build-template.json", "w")
+write_template_file(hf)
+
+if (args.b):
+	bf = open(f"{args.b}.json", "w")
+	write_build_file(bf)
+
+

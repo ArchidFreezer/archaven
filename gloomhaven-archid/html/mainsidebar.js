c7b08937e4a761cd41b45c615fd1e8d09572bec6
Archid <0archid0@gmail.com>
2025-11-30 13:44:15 +0000

    Add Tinkerer Damage build for archid mod

--- gloomhaven-archid/html/mainsidebar.js
+++ gloomhaven-archid/html/mainsidebar.js
@@ -172,6 +172,7 @@ function sidebarMainDropClick(id) {
     <button class="w3-button" onclick="sidebarMainDropClick('tiDrop')">Tinkerer <i class="fa fa-caret-down"></i></button>
     <div id="tiDrop" class="w3-dropdown-content w3-bar-block w3-white w3-card">
       <a href="ti-main.html" class="w3-bar-item w3-button">Overview</a>
+      <a href="ti-damage.html" class="w3-bar-item w3-button">Damage</a>
     </div>
   </div>
   <div class="w3-dropdown-click">
